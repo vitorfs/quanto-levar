c739a1fdad84f1a176cacb70edf750cd5386472d
bastosbf <bastosbf@gmail.com>
2014-04-14 21:11:38 -0300

    Adicionando auto-complete

--- static/js/app.js
+++ static/js/app.js
@@ -3,7 +3,6 @@ var LOADING = "<img src='/static/img/loading.gif' class='loading'>";
 $(function () {
   $("#cidade").click(function () {
     $(this).html(LOADING);
-    $(this).prop("disabled", true);
     return false;
   });
 });
